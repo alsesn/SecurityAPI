0ec3b3457ce179f87ca89a36ab6316e53c5c8155
alsesn <rostislavsusak747@gmail.com>
2025-03-13 01:53:54 +0200

    project update: 1. update EmployeeController 2. added thymeleaf dependencies 3. added list_employees

--- src/main/java/me/alsesn/SecurityAPI/config/SecurityConfig.java
+++ src/main/java/me/alsesn/SecurityAPI/config/SecurityConfig.java
@@ -1,45 +1,45 @@
-package me.alsesn.SecurityAPI.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
-
-@Configuration
-public class SecurityConfig {
-    @Bean
-    public UserDetailsManager userDetailsManager(DataSource dataSource) {
-        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
-
-        jdbcUserDetailsManager.setUsersByUsernameQuery(
-                "select user_id, pw, active from members where user_id=?");
-        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
-                "select roles.user_id, roles.role from roles where user_id=?");
-        
-        return jdbcUserDetailsManager;
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(configer -> configer
-                .requestMatchers(HttpMethod.GET, "/api/empls").hasRole("EMPLOYEE")
-                .requestMatchers(HttpMethod.GET, "/api/empls/**").hasRole("EMPLOYEE")
-                .requestMatchers(HttpMethod.POST, "/api/empls").hasRole("MANAGER")
-                .requestMatchers(HttpMethod.PUT, "/api/empls").hasRole("MANAGER")
-                .requestMatchers(HttpMethod.DELETE, "/api/empls/**").hasRole("ADMIN")
-        );
-        http.httpBasic(Customizer.withDefaults());
-        http.csrf(csfr -> csfr.disable());
-
-        return http.build();
-    }
-}
+//package me.alsesn.SecurityAPI.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.provisioning.JdbcUserDetailsManager;
+//import org.springframework.security.provisioning.UserDetailsManager;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//import javax.sql.DataSource;
+//
+//@Configuration
+//public class SecurityConfig {
+//    @Bean
+//    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+//        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+//
+//        jdbcUserDetailsManager.setUsersByUsernameQuery(
+//                "select user_id, pw, active from members where user_id=?");
+//        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+//                "select roles.user_id, roles.role from roles where user_id=?");
+//
+//        return jdbcUserDetailsManager;
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests(configer -> configer
+//                .requestMatchers(HttpMethod.GET, "/api/empls").hasRole("EMPLOYEE")
+//                .requestMatchers(HttpMethod.GET, "/api/empls/**").hasRole("EMPLOYEE")
+//                .requestMatchers(HttpMethod.POST, "/api/empls").hasRole("MANAGER")
+//                .requestMatchers(HttpMethod.PUT, "/api/empls").hasRole("MANAGER")
+//                .requestMatchers(HttpMethod.DELETE, "/api/empls/**").hasRole("ADMIN")
+//        );
+//        http.httpBasic(Customizer.withDefaults());
+//        http.csrf(csfr -> csfr.disable());
+//
+//        return http.build();
+//    }
+//}
--- src/main/java/me/alsesn/SecurityAPI/controller/EmployeeController.java
+++ src/main/java/me/alsesn/SecurityAPI/controller/EmployeeController.java
@@ -3,13 +3,13 @@ package me.alsesn.SecurityAPI.controller;
 import me.alsesn.SecurityAPI.entity.Employee;
 import me.alsesn.SecurityAPI.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/api")
 public class EmployeeController {
     private EmployeeService service;
@@ -19,40 +19,13 @@ public class EmployeeController {
         this.service = service;
     }
 
-    @GetMapping("/empls")
-    public List<Employee> findAll() {
-        return service.findAll();
-    }
+    @GetMapping("/list")
+    public String listEmployees(Model model) {
+        List<Employee> employees = service.findAll();
+        model.addAttribute("employees", employees);
 
-    @GetMapping("/empls/{id}")
-    public Employee findById(@PathVariable int id) {
-        Employee employee = service.findById(id);
-        if (employee == null) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Employee with id " + id + " not found");
-        } else {
-            return employee;
-        }
-    }
+        System.out.println(employees);
 
-    @PostMapping("/empls")
-    public Employee addEmployee(@RequestBody Employee employee) {
-        return service.save(employee);
-    }
-
-    @PutMapping("/empls")
-    public Employee updateEmployee(@RequestBody Employee employee) {
-        Employee updateEmployee = service.save(employee);
-        return updateEmployee;
-    }
-
-    @DeleteMapping("/empls/{id}")
-    public String deleteById(@PathVariable int id ) {
-        Employee employee = service.findById(id);
-        if (employee == null) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Employee with id " + id + " not found");
-        } else {
-            service.deleteById(id);
-        }
-        return "Deleted " + employee.toString();
+        return "list_employees";
     }
 }
